abfd377cab307f12e169453861a5ea9c56c2f20c
Gvile <guillaumevillemart@gmail.com>
2024-11-10 15:15:03 +0100

    connect api to bdd + add user routes

--- Esportify.Api/Program.cs
+++ Esportify.Api/Program.cs
@@ -1,14 +1,35 @@
+using Esportify.Api.App;
+using Esportify.Api.Map;
+using Esportify.Api.Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresqlConnection")));
+
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IEventRepository, EventRepository>();
+builder.Services.AddScoped<IEventStatusRepository, EventStatusRepository>();
+builder.Services.AddScoped<IEventImageRepository, EventImageRepository>();
+builder.Services.AddScoped<IEventUserRepository, EventUserRepository>();
+builder.Services.AddScoped<IRoleRepository, RoleRepository>();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.UseRouting();
+app.MapUserEndpoints();
 
 app.Run();
