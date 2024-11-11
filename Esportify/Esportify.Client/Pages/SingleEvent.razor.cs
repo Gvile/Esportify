55e31c65faa254f89e6bbdf9ab8af42826707f7c
Gvile <guillaumevillemart@gmail.com>
2024-11-11 17:15:22 +0100

    update singleEvent page

--- Esportify/Esportify.Client/Pages/SingleEvent.razor.cs
+++ Esportify/Esportify.Client/Pages/SingleEvent.razor.cs
@@ -12,13 +12,27 @@ public class SingleEventBase : ComponentBase
     
     [Inject] private NavigationManager _navigationManager { get; set; }
     [Inject] private EventService _eventService { get; set; }
+    [Inject] private UserService _userService { get; set; }
 
     protected EventModel Event { get; private set; } = new();
     
     protected override async Task OnInitializedAsync()
     {
         Event = await _eventService.GetByIdAsync(Id);
+        
+        var user = await _userService.GetByIdAsync(Event.OwnerUserId);
+        Event.OwnerUserPseudo = user.Pseudo;
     }
     
+    #endregion
+
+    #region Methods
+
+    protected string GetFormattedDate(DateTime date)
+    {
+        var culture = new System.Globalization.CultureInfo("fr-FR");
+        return date.ToString("dddd dd MMMM yyyy 'à' HH:mm", culture);
+    }
+
     #endregion
 }
