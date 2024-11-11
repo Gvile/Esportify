b2d00db4ee0f39d95f25991b52e3f36f3be64603
Gvile <guillaumevillemart@gmail.com>
2024-11-11 15:23:27 +0100

    add basic global event page behaviour

--- Esportify/Esportify.Client/Pages/GlobalEvent.razor.cs
+++ Esportify/Esportify.Client/Pages/GlobalEvent.razor.cs
@@ -1,7 +1,45 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Esportify.Shared.Model;
+using Esportify.Shared.Services;
+using Microsoft.AspNetCore.Components;
 
 namespace Esportify.Client.Pages;
 
 public class GlobalEventBase : ComponentBase
 {
+    #region Statements
+
+    [Inject] private NavigationManager _navigationManager { get; set; }
+    [Inject] private EventService _eventService { get; set; }
+    
+    protected List<EventModel> Events { get; set; } = new();
+
+    protected override async Task OnInitializedAsync()
+    {
+        Events = await _eventService.GetAllAsync();
+    }
+
+    #endregion
+
+    #region Methods
+
+    protected bool IsLiveEvent(EventModel evt)
+    {
+        var dateNow = DateTime.Now;
+
+        if (evt.StartDate < dateNow)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    protected void NavigateToSingleEventPage(int id)
+    {
+        _navigationManager.NavigateTo($"/SingleEvent/{id}");
+    }
+
+    #endregion
 }
