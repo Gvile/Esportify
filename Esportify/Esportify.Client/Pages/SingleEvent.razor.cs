3c3d47e0b17df88c933f81a7fb8888b61ea43c21
Gvile <guillaumevillemart@gmail.com>
2024-11-11 15:36:53 +0100

    add single global event page behaviour

--- Esportify/Esportify.Client/Pages/SingleEvent.razor.cs
+++ Esportify/Esportify.Client/Pages/SingleEvent.razor.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Esportify.Shared.Model;
+using Esportify.Shared.Services;
+using Microsoft.AspNetCore.Components;
 
 namespace Esportify.Client.Pages;
 
@@ -7,6 +9,16 @@ public class SingleEventBase : ComponentBase
     #region Statements
 
     [Parameter] public int Id { get; set; }
+    
+    [Inject] private NavigationManager _navigationManager { get; set; }
+    [Inject] private EventService _eventService { get; set; }
 
+    protected EventModel Event { get; private set; } = new();
+    
+    protected override async Task OnInitializedAsync()
+    {
+        Event = await _eventService.GetByIdAsync(Id);
+    }
+    
     #endregion
 }
