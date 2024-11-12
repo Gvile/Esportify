f1cde98c97932f5d29293e1e95b8ca6fe07fee6c
Gvile <guillaumevillemart@gmail.com>
2024-11-12 15:03:40 +0100

    add subscription

--- Esportify/Esportify.Client/Pages/GlobalEvent.razor.cs
+++ Esportify/Esportify.Client/Pages/GlobalEvent.razor.cs
@@ -11,8 +11,12 @@ public class GlobalEventBase : ComponentBase
     [Inject] private NavigationManager _navigationManager { get; set; }
     [Inject] private EventService _eventService { get; set; }
     [Inject] private UserService _userService { get; set; }
+    [Inject] private EventUserService _eventUserService { get; set; }
+    
+    private UserModel _user;
     
     protected List<EventModel> Events { get; set; } = new();
+    protected List<EventUserModel> EventUsers { get; set; } = new();
 
     protected override async Task OnInitializedAsync()
     {
@@ -23,6 +27,10 @@ public class GlobalEventBase : ComponentBase
             var user = await _userService.GetByIdAsync(evt.OwnerUserId);
             evt.OwnerUserPseudo = user.Pseudo;
         }
+        
+        EventUsers = await _eventUserService.GetAllAsync();
+
+        _user = await _userService.GetByIdAsync(1);
     }
 
     #endregion
@@ -41,6 +49,34 @@ public class GlobalEventBase : ComponentBase
         {
             return false;
         }
+    }
+
+    protected async Task OnSubscription(EventModel evt)
+    {
+        var eventUsers = await _eventUserService.GetAllAsync();
+
+        var isExist = false;
+        foreach (var eventUser in eventUsers)
+        {
+            if (eventUser.EventId == evt.Id && eventUser.UserId == _user.Id)
+            {
+                isExist = true;
+                break;
+            }
+        }
+
+        if (!isExist)
+        {
+            var eventUser = new EventUserModel(evt.Id, _user.Id);
+            EventUsers.Add(await _eventUserService.CreateAsync(eventUser));
+            StateHasChanged();
+        }
+    }
+
+    protected bool IsSubscribed(int evtId)
+    {
+        var eventUser = EventUsers.FirstOrDefault(e => e.EventId == evtId && e.UserId == _user.Id);
+        return eventUser != null;
     }
     
     protected void NavigateToSingleEventPage(int id)
--- Esportify/Esportify.Client/Pages/SingleEvent.razor.cs
+++ Esportify/Esportify.Client/Pages/SingleEvent.razor.cs
@@ -13,8 +13,12 @@ public class SingleEventBase : ComponentBase
     [Inject] private NavigationManager _navigationManager { get; set; }
     [Inject] private EventService _eventService { get; set; }
     [Inject] private UserService _userService { get; set; }
+    [Inject] private EventUserService _eventUserService { get; set; }
 
     protected EventModel Event { get; private set; } = new();
+    protected List<EventUserModel> EventUsers { get; set; } = new();
+    
+    private UserModel _user;
     
     protected override async Task OnInitializedAsync()
     {
@@ -22,11 +26,43 @@ public class SingleEventBase : ComponentBase
         
         var user = await _userService.GetByIdAsync(Event.OwnerUserId);
         Event.OwnerUserPseudo = user.Pseudo;
+        
+        EventUsers = await _eventUserService.GetAllAsync();
+        
+        _user = await _userService.GetByIdAsync(1);
     }
     
     #endregion
 
     #region Methods
+    
+    protected async Task OnSubscription()
+    {
+        var eventUsers = await _eventUserService.GetAllAsync();
+
+        var isExist = false;
+        foreach (var eventUser in eventUsers)
+        {
+            if (eventUser.EventId == Event.Id && eventUser.UserId == _user.Id)
+            {
+                isExist = true;
+                break;
+            }
+        }
+
+        if (!isExist)
+        {
+            var eventUser = new EventUserModel(Event.Id, _user.Id);
+            EventUsers.Add(await _eventUserService.CreateAsync(eventUser));
+            StateHasChanged();
+        }
+    }
+    
+    protected bool IsSubscribed()
+    {
+        var eventUser = EventUsers.FirstOrDefault(e => e.EventId == Event.Id && e.UserId == _user.Id);
+        return eventUser != null;
+    }
 
     protected string GetFormattedDate(DateTime date)
     {
